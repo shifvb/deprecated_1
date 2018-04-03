5cc5169afb18795eec5522367aeb7a1ccf2fa75d
shifvb <shifvb@gmail.com>
2018-04-03 18:26:15 +0800

    将存储路径拓展名从.tif改为.png

--- DIRNet_tensorflow_master/models.py
+++ DIRNet_tensorflow_master/models.py
@@ -78,9 +78,9 @@ class DIRNet(object):
     def deploy(self, dir_path, x, y):
         z = self.sess.run(self.z, {self.x: x, self.y: y})
         for i in range(z.shape[0]):
-            save_image_with_scale(dir_path + "/{:02d}_x.tif".format(i + 1), x[i, :, :, 0])
-            save_image_with_scale(dir_path + "/{:02d}_y.tif".format(i + 1), y[i, :, :, 0])
-            save_image_with_scale(dir_path + "/{:02d}_z.tif".format(i + 1), z[i, :, :, 0])
+            save_image_with_scale(dir_path + "/{:02d}_x.png".format(i + 1), x[i, :, :, 0])
+            save_image_with_scale(dir_path + "/{:02d}_y.png".format(i + 1), y[i, :, :, 0])
+            save_image_with_scale(dir_path + "/{:02d}_z.png".format(i + 1), z[i, :, :, 0])
 
     def save(self, dir_path):
         self.vCNN.save(self.sess, dir_path + "/model.ckpt")

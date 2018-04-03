05ccdbf91b745e55b410695a8c5fa5b2d4574339
shifvb <shifvb@gmail.com>
2018-04-03 19:58:50 +0800

    修改生成图像对算法

--- DIRNet_tensorflow_master/source/data/gen_ct_patches.py
+++ DIRNet_tensorflow_master/source/data/gen_ct_patches.py
@@ -19,7 +19,7 @@ def _gen_batches(arr: list, middle_length: int = 200):
     _ = int((len(arr) - middle_length) / 2)
     for img_index in range(_, _ + middle_length, 2):
         batch_x.append(arr[img_index])
-        batch_y.append(arr[img_index + 1])
+        batch_y.append(arr[img_index + 3])
     return batch_x, batch_y
 
 
@@ -31,20 +31,19 @@ def gen_batches(workspaces: list):
         ct_workspace = os.path.join(workspace, "4")
         ct_filenames = [os.path.join(ct_workspace, _) for _ in os.listdir(ct_workspace) if _.startswith("CT_")]
         ct_filenames.sort(key=lambda _: int(_.split("_")[-1]))
-        _x, _y = _gen_batches([pydicom.read_file(_).pixel_array for _ in ct_filenames], middle_length=200)
+        _x, _y = _gen_batches([pydicom.read_file(_).pixel_array for _ in ct_filenames], middle_length=150)
         batch_x.extend(_x)
         batch_y.extend(_y)
     batch_x = np.stack(batch_x, axis=0)
     batch_y = np.stack(batch_y, axis=0)
     batch_x = batch_x.reshape(*batch_x.shape, 1)
     batch_y = batch_y.reshape(*batch_y.shape, 1)
-
     # generate batches
     return batch_x, batch_y
 
 
 def main():
-    workspaces = [os.path.join(r"F:\registration", _) for _ in os.listdir(r"F:\registration")]
+    workspaces = [os.path.join(r"F:\registration", _) for _ in os.listdir(r"F:\registration") if not _.endswith("ckle")]
     _ = gen_batches(workspaces)
     out_path = r"F:\registration\ct_batches.pickle"
     if not os.path.exists(out_path):

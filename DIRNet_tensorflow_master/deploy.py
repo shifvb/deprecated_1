2abaa34966be027a373492fc22194342a18fd8d3
shifvb <shifvb@gmail.com>
2018-04-03 18:26:54 +0800

    添加了自己的数据用于配准

--- DIRNet_tensorflow_master/deploy.py
+++ DIRNet_tensorflow_master/deploy.py
@@ -1,5 +1,7 @@
 import os
 import tensorflow as tf
+from PIL import Image
+import numpy as np
 from DIRNet_tensorflow_master.models import DIRNet
 from DIRNet_tensorflow_master.config import get_config
 from DIRNet_tensorflow_master.data import MNISTDataHandler
@@ -28,5 +30,37 @@ def main():
         reg.deploy(result_i_dir, batch_x, batch_y)
 
 
+def register_img(moving_image, fixed_image):
+    config_dict = {
+        "checkpoint_dir": "checkpoint",
+        "image_size": [64, 64],
+        "batch_size": 1,
+        "result_dir": "result",
+    }
+
+    # file operations
+    if not os.path.exists(config_dict["result_dir"]):
+        os.mkdir(config_dict["result_dir"])
+
+    # create network
+    sess = tf.Session()
+    reg = DIRNet(sess, config_dict, "DIRNet", is_train=False)
+    reg.restore(config_dict["checkpoint_dir"])
+
+    # register
+    batch_x = np.array([moving_image])
+    batch_y = np.array([fixed_image])
+    reg.deploy(config_dict["result_dir"], batch_x, batch_y)
+
+
 if __name__ == "__main__":
-    main()
+    x = r"C:\Users\anonymous\Desktop\workspace\moving_image.jpg"
+    y = r"C:\Users\anonymous\Desktop\workspace\fixed_image.jpg"
+    x_arr = np.array(Image.open(x)).reshape([64, 64, 1])
+    y_arr = np.array(Image.open(y)).reshape([64, 64, 1])
+    Image.fromarray(x_arr[:, :, 0]).show()
+    Image.fromarray(y_arr[:, :, 0]).show()
+    x_arr = x_arr / 255
+    y_arr = y_arr / 255
+    register_img(x_arr, y_arr)
+    # main()

c85d0e70051e4a1321ac0c2a3ff5baac9e94bd7a
shifvb <shifvb@gmail.com>
2018-04-18 09:40:47 +0800

    update

--- DIRNet_tensorflow_master/data/analyze/gen_loss_graph_from_log.py
+++ DIRNet_tensorflow_master/data/analyze/gen_loss_graph_from_log.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    workspace = r"F:\registration_results_backup\2018_04_16_17_00_实验二_向下移动11像素(获取数组reg.v)_imgnum=60410_imgsize=128x128_batch=80_iter=10000\log"
+    workspace = r"F:\registration_results_backup\2018_04_17_11_37_实验三_使用了源程序自带的mse作为loss, 图像水平垂直移动8~13像素)_imgnum=60410_imgsize=128x128_batch=80_iter=10000\log"
     with open(os.path.join(workspace, "train.log"), 'r') as f:
         s = f.read()
 
@@ -11,6 +11,23 @@ def main():
     s = [float(_.split(":")[-1]) for _ in s]
     assert 10000 == len(s), len(s)  # 默认训练次数
     plt.plot(range(len(s)), s)
+    plt.show()
+
+
+def gen_graph_from_loss_ver2():
+    workspace = r"F:\registration_results_backup\2018_04_18_08_44_实验五_(使用了NCC+vCNN输出的灰度值方差作为loss函数，图像水平或垂直移动8~13像素)_imgnum=60410_imgsize=128x128_batch=80_iter=10000\log"
+    with open(os.path.join(workspace, "train.log"), 'r') as f:
+        s = f.read()
+    s = [_ for _ in s.split('\n') if not _ == ""]
+    s1 = [float(_.split(",")[1].split("=")[1]) for _ in s]
+    s2 = [float(_.split(",")[2].split("=")[1]) for _ in s]
+    s3 = [float(_.split(",")[3].split("=")[1]) for _ in s]
+    print(s1)
+    print(s2)
+    print(s3)
+    plt.plot(range(len(s1)), s1, c='red')
+    plt.plot(range(len(s2)), s2, c='blue')
+    plt.plot(range(len(s3)), s3, c='green')
     plt.show()
 
 

021f5f28c85e99595b3ea76cd71090cab4688f34
shifvb <shifvb@gmail.com>
2018-04-19 09:48:48 +0800

    update(stage final)

--- DIRNet_tensorflow_master/train/train_config.py
+++ DIRNet_tensorflow_master/train/train_config.py
@@ -8,9 +8,9 @@ def train_config_v1():
         "batch_folder": r"F:\registration_patches\向水平或竖直方向移动8-13像素\train",
         # train parameters
         "image_size": [128, 128],
-        "batch_size": 10,
-        "learning_rate": 1e-4,
-        "iteration_num": 10000,
+        "batch_size": 80,
+        "learning_rate": 1e-5,
+        "iteration_num": 20000,
         # train data folder
         "checkpoint_dir": r"F:\registration_running_data\checkpoints",
         "temp_dir": r"F:\registration_running_data\temp",

75e13150445c0e00080f1f1b468361132b11075d
shifvb <shifvb@gmail.com>
2018-04-24 20:45:46 +0800

    update model

--- 日文论文实现/train/train.py
+++ 日文论文实现/train/train.py
@@ -1,21 +1,74 @@
 import os
 import tensorflow as tf
-from 日文论文实现.models.R1 import R1, R2, R3,ConvNetRegressor
+from 日文论文实现.models.R1 import R1, R2, R3, ConvNetRegressor
 
 
-def main():
-    config_dict = {
+def train():
+    config_dict = config_folder_guard({
+        # network settings
         "batch_size": 3,
         "img_height": 512,
         "img_width": 512,
-        "learning_rate": 1e-5,
-    }
-    net = ConvNetRegressor(None, is_train=True, config=config_dict)
 
-    # xy = tf.placeholder(dtype=tf.float32, shape=[10, 512, 512, 2])
-    # for x in r:
-    #     print(x)
+        # train parameters
+        "epoch_num": 10000,
+        "learning_rate": 1e-5,
+
+        # folder path
+        "checkpoint_folder": "",  # todo remove it
+        "validate_visualization_folder": "",  # todo change it
+    })
+
+    sess = tf.Session()
+    reg = ConvNetRegressor(sess, is_train=True, config=config_dict)
+    for i in range(config_dict["epoch_num"]):
+        batch_x, batch_y = get_train_batches(config_dict["batch_size"])  # todo change it
+        loss = reg.fit(batch_x, batch_y)
+        print("[INFO] epoch={:>5}, loss={:.3f}".format(i, loss))
+        if (i + 1) % 1000 == 0:
+            _validate_batch_x, _validate_batch_y = get_validate_batches(config_dict["batch_size"])
+            reg.deploy(config_dict["validate_visualization_folder"], _validate_batch_x, _validate_batch_y)
+            reg.save(sess, config_dict["checkpoint_folder"])
+
+
+def test():
+    config_dict = config_folder_guard({
+        # network settings
+        "batch_size": 3,
+        "img_height": 512,
+        "img_width": 512,
+
+        # folder path
+        "checkpoint_folder": "",  # todo remove it
+        "test_visualization_folder": "",  # todo change it
+    })
+    sess = tf.Session()
+    reg = ConvNetRegressor(sess, is_train=False, config=config_dict)
+    reg.restore(sess, config_dict["checkpoint_folder"])
+    batch_x, batch_y = get_test_batches(config_dict["batch_size"])
+    reg.deploy(config_dict["test_visualization_folder"], batch_x, batch_y)
+
+
+def config_folder_guard(config_dict: dict):
+    """防止出现文件夹不存在的情况"""
+    pass  # todo: change it, do some guard things
+    return config_dict
+
+
+def get_validate_batches(batch_size: int):
+    batch_x, batch_y = None, None  # todo change it
+    return batch_x, batch_y
+
+
+def get_train_batches(batch_size: int):
+    batch_x, batch_y = None, None  # todo change it
+    return batch_x, batch_y
+
+
+def get_test_batches(batch_size: int):
+    batch_x, batch_y = None, None  # todo change it
+    return batch_x, batch_y
 
 
 if __name__ == '__main__':
-    main()
+    train()

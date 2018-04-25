baa13f404fd64927f2c4f83dc979dc93eb327e04
shifvb <shifvb@gmail.com>
2018-04-25 16:17:10 +0800

    update model

--- DIRNet_tensorflow_master/deprecated/deprecated_mygrid&TEO.py
+++ DIRNet_tensorflow_master/deprecated/deprecated_mygrid&TEO.py
@@ -3,7 +3,7 @@ from DIRNet_tensorflow_master.train.train_exchange_obj import TEO
 from DIRNet_tensorflow_master.train.train_exchange_obj import TEO
 
 # ---------------------------- WrapST.py ------------------------------
-# my grid start # todo remove it
+# my grid start
 _mygrid = _meshgrid(out_height, out_width)  # [2, h*w]
 _mygrid = tf.reshape(_mygrid, [-1])  # [2*h*w]
 _mygrid = tf.tile(_mygrid, tf.stack([num_batch]))  # [n*2*h*w]
@@ -14,8 +14,8 @@ _my_x_s_flat = tf.reshape(_my_x_s, [-1])
 _my_y_s_flat = tf.reshape(_my_y_s, [-1])
 _my_x_s_flat = tf.cast((_my_x_s_flat + 1) * (width_f) / 2, 'int32')
 _my_y_s_flat = tf.cast((_my_y_s_flat + 1) * (height_f) / 2, 'int32')
-TEO.x_diff = tf.cast(x0 - _my_x_s_flat, 'float32')  # todo remove it
-TEO.y_diff = tf.cast(y0 - _my_y_s_flat, 'float32')  # todo remove it
+TEO.x_diff = tf.cast(x0 - _my_x_s_flat, 'float32')
+TEO.y_diff = tf.cast(y0 - _my_y_s_flat, 'float32')
 
 
 # ------------------------ train_exchange_obj(TEO).py ------------------------
@@ -26,7 +26,6 @@ class TEO(object):
 # ------------------------- model.py ---------------------------
 # ncc: from 0.1 -> 0.8, so -ncc is from -0.1 -> -0.8, can be `minimized`
 self.loss_term_1 = -ncc(self.y, self.z)
-# todo: my version of loss
 _mean_x = tf.reduce_mean(TEO.x_diff)
 _variance_x = tf.reduce_mean(tf.square(TEO.x_diff - _mean_x))
 _mean_y = tf.reduce_mean(TEO.y_diff)

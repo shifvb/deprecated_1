b1ae489fd53c70f688492f776e256dff04c6c62c
shifvb <shifvb@gmail.com>
2018-04-26 17:58:20 +0800

    update examples

---
+++ learn_tensorflow/slice_input_producer_and_batch_examples/slice_input_producer_and_batch.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+import tensorflow as tf
+
+
+def get_filenames_and_labels(file_dir: str):  # 得到cats和dogs的图像路径(shape=[25000, ])和标签(shape=[25000, ])
+    _cats, _label_cats, _dogs, _label_dogs = [], [], [], []
+    # get cats abs filename & label
+    cat_dir = os.path.join(file_dir, "Cat")
+    for filename in os.listdir(cat_dir):
+        _cats.append(os.path.join(cat_dir, filename))
+        _label_cats.append(0)
+    _cats.sort(key=lambda _: int(os.path.split(_)[-1].split(".")[0]))
+    # get dogs abs filename & label
+    dog_dir = os.path.join(file_dir, "Dog")
+    for filename in os.listdir(dog_dir):
+        _dogs.append(os.path.join(dog_dir, filename))
+        _label_dogs.append(1)
+    _dogs.sort(key=lambda _: int(os.path.split(_)[-1].split(".")[0]))
+    return np.hstack([_cats, _dogs]), np.hstack([_label_cats, _label_dogs])
+
+
+def main():
+    batch_size = 1
+    shuffle = False
+    epoch_num = 10000
+
+    # 生成图片集和标签
+    image_arr, label_arr = get_filenames_and_labels(r'F:\kagglecatsanddogs_3367a\PetImages')
+    input_queue = tf.train.slice_input_producer([image_arr, label_arr], shuffle=shuffle)
+    image_batch, label_batch = tf.train.batch(input_queue, batch_size=batch_size)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        # train
+        for i in range(epoch_num):
+            img, label = sess.run([image_batch, label_batch])
+            print("iter={}, x={}, y={}".format(i, img, label))
+
+        coord.request_stop()
+        coord.join(threads)
+
+
+if __name__ == '__main__':
+    main()

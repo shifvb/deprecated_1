e5b64cb8e4c3f305aa1066c8566d580ec622309a
shifvb <shifvb@gmail.com>
2018-04-26 22:12:38 +0800

    Change DIRNet

--- DIRNet_tensorflow_master/train/train.py
+++ DIRNet_tensorflow_master/train/train.py
@@ -6,23 +6,47 @@ import tensorflow as tf
 from DIRNet_tensorflow_master.models.models import DIRNet
 from DIRNet_tensorflow_master.data.log import my_logger
 from DIRNet_tensorflow_master.train.batches_generator import Batches, sample_pair
+from learn_tensorflow.slice_input_producer_and_batch_examples.slice_input_producer_and_batch import \
+    get_filenames_and_labels
+from PIL import Image
+
+
+def f(input_tensor):
+    L = []
+    for i in input_tensor:
+        L.append(np.array(Image.open(i), dtype=np.uint8))
+    return np.stack(L)
 
 
 def my_train(config: dict):
-    # 加载数据
-    batch_filenames = [os.path.join(config["batch_folder"], _) for _ in os.listdir(config["batch_folder"])]
-    batches = Batches(config["iteration_num"], batch_filenames)
+    # 生成图片集和标签
+    x_arr = r"F:\新建文件夹\resized_ct"
+    x_arr = [os.path.join(x_arr, _) for _ in os.listdir(x_arr)]
+    y_arr = r"F:\新建文件夹\shift_10_10_ct"
+    y_arr = [os.path.join(y_arr, _) for _ in os.listdir(y_arr)]
+    input_queue = tf.train.slice_input_producer([x_arr, y_arr], shuffle=config["shuffle_batch"])
+    batch_x, batch_y = tf.train.batch(input_queue, batch_size=config["batch_size"])
+    batch_x = tf.py_func(f, [batch_x], tf.uint8)
+    batch_y = tf.py_func(f, [batch_y], tf.uint8)
+    batch_x = tf.reshape(batch_x, [config["batch_size"], 128, 128, 1])
+    batch_y = tf.reshape(batch_y, [config["batch_size"], 128, 128, 1])
+    batch_x = batch_x / 255
+    batch_y = batch_y / 255
 
     with tf.Session() as sess:
         reg = DIRNet(sess, config, "DIRNet", is_train=True)
         sess.run(tf.global_variables_initializer())
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
         for i in range(config["iteration_num"]):
-            batch_x, batch_y = sample_pair(*(batches.get_batches(i)), config["batch_size"])
-            loss = reg.fit(batch_x, batch_y)
+            _bx, _by = sess.run([batch_x, batch_y])
+            loss = reg.fit(_bx, _by)
             config["logger"].info("iter={:>6d}, loss={:.6f}".format(i + 1, loss))
-            if (i + 1) % 1000 == 0:
-                reg.deploy(config["temp_dir"], batch_x, batch_y)
+            if (i + 1) % 100 == 0:
+                reg.deploy(config["temp_dir"], _bx, _by)
                 reg.save(config["checkpoint_dir"])
+        coord.request_stop()
+        coord.join(threads)
 
 
 def main():
@@ -34,6 +58,7 @@ def main():
         "batch_size": 10,
         "learning_rate": 1e-5,
         "iteration_num": 20000,
+        "shuffle_batch": False,
         # train data folder
         "checkpoint_dir": r"F:\registration_running_data\checkpoints",
         "temp_dir": r"F:\registration_running_data\temp",

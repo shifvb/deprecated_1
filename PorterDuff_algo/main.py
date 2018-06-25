cb547d40fe29f96e0a4d0ad3f8fc4b7b8fa13740
chipsbear <zhangshunbonev@163.com>
2018-06-25 20:06:38 +0800

    update some trial work

--- PorterDuff_algo/main.py
+++ PorterDuff_algo/main.py
@@ -5,42 +5,28 @@ from PorterDuff_algo.PorterDuff import PorterDuff
 from PorterDuff_algo.gray2color import gray2color
 
 
-def main():
-    mov_path = r"C:\Users\anonymous\Desktop\2\mov.png"
-    fix_path = r"C:\Users\anonymous\Desktop\2\fix.png"
-    mov_img = Image.open(mov_path).convert(mode="RGBA")
-    fix_img = Image.open(fix_path).convert(mode="RGBA")
+def main(mov_arr, fix_arr):
+    mov_arr_c = mov_arr[:, :, :3]
+    mov_arr_a = mov_arr[:, :, 3:]
 
-    mov_arr = np.array(mov_img)
-    fix_arr = np.array(fix_img)
+    # 去除暗的
+    # low_gray_level_pos = mov_arrmov_arr[:, :, :3]
 
+    # 归一化
+    mov_arr_c = mov_arr_c / (mov_arr_c.max() - mov_arr_c.min()) * 255
 
-    mov_arr[:, :, :3] = mov_arr[:, :, :3] / (mov_arr[:, :, :3].max() - mov_arr[:, :, :3].min()) * 255
-    # mov_arr[:, :, :3] =np.clip((mov_arr[:, :, :3].astype(np.int32)) + 120, 0, 255).astype(np.uint8)
-    mov_arr = mov_arr.astype(np.uint8)
-    mov_arr[:, :, :3] = gray2color(mov_arr[:, :, 2])  # R
+    # 生成伪彩色
+    mov_arr_c = mov_arr_c.astype(np.uint8)
+    mov_arr_c = gray2color(mov_arr_c[:, :, 0])  # R
 
-    # fix_arr[:, :, :3] = fix_arr[:, :, :3] / (fix_arr[:, :, :3].max() - fix_arr[:, :, :3].min()) * 255
-    # fix_arr = mov_arr.astype(np.uint8)
-    # fix_arr[:, :, :3] = gray2color(fix_arr[:, :, 2])  # R
+    # 调整alpha值
+    mov_arr_a = np.ones_like(mov_arr_a, dtype=np.float32) * 255 * 0.4
+    mov_arr = np.concatenate([mov_arr_c, mov_arr_a], axis=2)
+    print(mov_arr.dtype)
 
-    mov_arr = mov_arr.astype(np.float32)
-    fix_arr = fix_arr.astype(np.float32)
-
-    # alpha
-    mov_arr[:, :, 3] = 255 * 0.4
-    fix_arr[:, :, 3] = 255 * 1
-    # # # R
-    # mov_arr[:, :, 1] = 0
-    # mov_arr[:, :, 2] = 0
-    # # # G
-    # fix_arr[:, :, 0] = 0
-    # fix_arr[:, :, 2] = 0
-    print(mov_arr.shape, mov_arr.dtype)
-    print(fix_arr.shape, fix_arr.dtype)
-
+    # 图像融合
     pd = PorterDuff(mov_arr, fix_arr)
-    out_arr = pd.alpha_composition(mode=PorterDuff.XOR)
+    out_arr = pd.alpha_composition(mode=PorterDuff.DARKEN)
     Image.fromarray(mov_arr.astype(np.uint8)).save(r"C:\Users\anonymous\Desktop\2\mov_out.png")
     Image.fromarray(fix_arr.astype(np.uint8)).save(r"C:\Users\anonymous\Desktop\2\fix_out.png")
     Image.fromarray(out_arr).save(r"C:\Users\anonymous\Desktop\2\out.png")
@@ -48,4 +34,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    mov_path = r"C:\Users\anonymous\Desktop\2\mov.png"
+    fix_path = r"C:\Users\anonymous\Desktop\2\fix.png"
+    mov_img = Image.open(mov_path).convert(mode="RGBA")
+    fix_img = Image.open(fix_path).convert(mode="RGBA")
+    main(np.array(mov_img), np.array(fix_img))

2647440254b89570bc8f036516dca2022b2ee586
shifvb <shifvb@gmail.com>
2018-07-04 12:10:26 +0800

    update DIRNet_3d train processes

--- DIRNet3D_for_PETCT_images/train/train.py
+++ DIRNet3D_for_PETCT_images/train/train.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 from DIRNet3D_for_PETCT_images.models.DIRNet_3d import DIRNet3D
 from DIRNet3D_for_PETCT_images.train.train_utils import my_logger as logger
 from DIRNet3D_for_PETCT_images.train.train_utils import LossRecorder
+from DIRNet3D_for_PETCT_images.data.sample_data import MyBatch as Batch
 
 
 class TrainConfig(object):
@@ -18,6 +19,10 @@ class TrainConfig(object):
         self.epoch_num = 50
         self.train_per_epoch = 400
         self.valid_iter_num = len(os.listdir(valid_y_dir)) // batch_size
+
+        # 数据设置
+        self.train_data_set = Batch(train_x_dir, train_y_dir, self.batch_size, True)
+        self.valid_data_set = Batch(valid_x_dir, valid_y_dir, self.batch_size, False)
 
         # 存储路径设置
         self.temp_dir = temp_dir
@@ -47,7 +52,7 @@ def main():
     for epoch in range(cfg.epoch_num):
         # 开始单个epoch训练
         for i in range(cfg.train_per_epoch):
-            _tx, _ty = train_dataset.next_batch()  # 随机获得训练集
+            _tx, _ty = cfg.train_data_set.next_batch()  # 随机获得训练集
             cfg.loss_rec.record_loss(*net.fit(_tx, _ty))  # 记录训练loss
         # 记录训练loss日志
         cfg.train_logger.info("[TRAIN] epoch={:>6d}, loss={:.6f}, "
@@ -56,7 +61,7 @@ def main():
         # 放入验证集进行验证
         _v_img_path = cfg.temp_dir if epoch % 5 == 0 else None
         for j in range(cfg.valid_iter_num):
-            _vx, _vy = valid_dataset.next_batch()  # 顺序获得训练集
+            _vx, _vy = cfg.valid_data_set.next_batch()  # 顺序获得训练集
             cfg.loss_rec.record_loss(*net.deploy(_vx, _vy, _v_img_path, j * cfg.batch_size))  # 记录验证loss
         # 记录验证loss日志
         cfg.valid_logger.info("[VALID] epoch={:>6d}, loss={:.6f}, "

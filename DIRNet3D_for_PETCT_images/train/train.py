428dd4953f740773fde98c889fb91bc5ddbbcc85
shifvb <shifvb@gmail.com>
2018-07-04 10:31:08 +0800

    update DIRNet_3d train processes

--- DIRNet3D_for_PETCT_images/train/train.py
+++ DIRNet3D_for_PETCT_images/train/train.py
@@ -1,3 +1,4 @@
+import os
 import time
 import numpy as np
 import tensorflow as  tf
@@ -7,17 +8,36 @@ from DIRNet3D_for_PETCT_images.train.train_utils import LossRecorder
 
 
 class TrainConfig(object):
-    def __init__(self):
-        self.learning_rate = 1e-4
+    def __init__(self, batch_size, learning_rate,
+                 train_x_dir, train_y_dir, valid_x_dir, valid_y_dir, temp_dir):
+        # 网络参数设置
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+
+        # 循环次数设置
         self.epoch_num = 50
         self.train_per_epoch = 400
+        self.valid_iter_num = len(os.listdir(valid_y_dir)) // batch_size
+
+        # 存储路径设置
+        self.temp_dir = temp_dir
         self.train_logger = logger(r"F:\registration_running_data\log", "train.log")
         self.valid_logger = logger(r"F:\registration_running_data\log", "valid.log")
+        self.loss_rec = LossRecorder()
 
 
 def main():
     # 网络参数设置
-    cfg = TrainConfig()
+    # todo: pathes
+    cfg = TrainConfig(
+        batch_size=32,
+        learning_rate=1e-4,
+        train_x_dir=r"",
+        train_y_dir=r"",
+        valid_x_dir=r"",
+        valid_y_dir=r"",
+        temp_dir=r"",
+    )
 
     # 构建网络
     sess = tf.Session()
@@ -26,17 +46,21 @@ def main():
     # 开始训练
     for epoch in range(cfg.epoch_num):
         # 开始单个epoch训练
-        loss_recorder = LossRecorder()  # 初始化loss recorder
         for i in range(cfg.train_per_epoch):
-            _tx, _ty = train_dataset.next_batch()
-            loss_recorder.record_loss(*net.fit(_tx, _ty))  # 记录loss
+            _tx, _ty = train_dataset.next_batch()  # 随机获得训练集
+            cfg.loss_rec.record_loss(*net.fit(_tx, _ty))  # 记录训练loss
         # 记录训练loss日志
-        cfg.train_logger.info(
-            "[TRAIN] epoch={:>6d}, loss={:.6f}, ncc_loss={:.6f}, grad_loss={:.6f}".format(
-                epoch, *loss_recorder.get_avg_losses()
-            )
-        )
-        # 单个epoch训练后放入验证集进行验证
+        cfg.train_logger.info("[TRAIN] epoch={:>6d}, loss={:.6f}, "
+                              "ncc_loss={:.6f}, grad_loss={:.6f}".format(epoch, *cfg.loss_rec.get_losses()))
+
+        # 放入验证集进行验证
+        _v_img_path = cfg.temp_dir if epoch % 5 == 0 else None
+        for j in range(cfg.valid_iter_num):
+            _vx, _vy = valid_dataset.next_batch()  # 顺序获得训练集
+            cfg.loss_rec.record_loss(*net.deploy(_vx, _vy, _v_img_path, j * cfg.batch_size))  # 记录验证loss
+        # 记录验证loss日志
+        cfg.valid_logger.info("[VALID] epoch={:>6d}, loss={:.6f}, "
+                              "ncc_loss={:.6f}, grad_loss={:.6f}".format(epoch, *cfg.loss_rec.get_losses()))
 
 
 if __name__ == '__main__':
--- DIRNet3D_for_PETCT_images/train/train_utils.py
+++ DIRNet3D_for_PETCT_images/train/train_utils.py
@@ -25,19 +25,26 @@ def my_logger(folder_name: str, file_name: str):
 
 
 class LossRecorder(object):
+    """用来记录loss的便捷类"""
+
     def __init__(self):
         self.L1 = list()
         self.L2 = list()
         self.L3 = list()
 
+    def _clear(self):
+        self.L1.clear()
+        self.L2.clear()
+        self.L3.clear()
+
     def record_loss(self, total_loss, ncc_loss, grad_loss):
+        """添加loss"""
         self.L1.append(total_loss)
         self.L2.append(ncc_loss)
         self.L3.append(grad_loss)
 
-    def get_avg_losses(self):
-        return (
-            sum(self.L1) / len(self.L1),
-            sum(self.L2) / len(self.L2),
-            sum(self.L3) / len(self.L3),
-        )
+    def get_losses(self):
+        """返回平均loss， *同时清除当前已有loss记录*"""
+        _losses = (sum(self.L1) / len(self.L1), sum(self.L2) / len(self.L2), sum(self.L3) / len(self.L3))
+        self._clear()
+        return _losses

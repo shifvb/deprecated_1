a3a55a027b582651e66bf5ae5346c45ec4eacdb0
shifvb <shifvb@gmail.com>
2018-07-08 17:17:59 +0800

    update analyze

--- DIRNet3D_for_PETCT_images/analyze/img_3D_to_2D.py
+++ DIRNet3D_for_PETCT_images/analyze/img_3D_to_2D.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 
 
-def _trans(in_path, out_folder):
+def _trans(in_path, out_folder, axis):
     # handle files % folders
     _name = os.path.split(in_path)[-1].split(".")[0]
     out_folder = os.path.join(out_folder, _name)
@@ -12,6 +12,9 @@ def _trans(in_path, out_folder):
         os.makedirs(out_folder)
     # extract data
     data = pickle.load(open(in_path, 'rb'))
+    if axis is not None:
+        data = data.transpose(axis)
+    print("[DEBUG] loaded {} with:\n\tshape: {}\n\tdtype:{}".format(in_path, data.shape, data.dtype))
     if data.ndim == 5:
         for i in range(data.shape[3]):
             _img_arr = (data[0, :, :, i, 0] * 255).astype(np.uint8)
@@ -25,15 +28,16 @@ def _trans(in_path, out_folder):
     # save data
 
 
-def trans(in_folder, out_folder):
+def trans(in_folder, out_folder, axis=None):
     img_names = [os.path.join(in_folder, _) for _ in os.listdir(in_folder)]
     for img_name in img_names:
-        _trans(img_name, out_folder)
+        _trans(img_name, out_folder, axis)
 
 
 def main():
-    trans(r"F:\registration_running_data\validate", r"F:\registration_running_data\view\validate")
-
+    trans(r"F:\新建文件夹", r"d:\result_hori")
+    trans(r"F:\新建文件夹", r"d:\result_sagi", [2, 0, 1])
+    trans(r"F:\新建文件夹", r"d:\result_coro", [2, 1, 0])
 
 if __name__ == '__main__':
     main()
